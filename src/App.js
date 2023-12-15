f8a99b7147be5d2af2fc2bb20b290f653610c771
Thiyash05 <sandyramar55@gmail.com>
2023-12-15 16:48:59 +0530

    add new

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Timer from "./Timer"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./Timer.css"
+
+function App() {
+  return (
+    <div className="App">
+      <Timer />
+    </div>
+  );
+}
+
+export default App;
